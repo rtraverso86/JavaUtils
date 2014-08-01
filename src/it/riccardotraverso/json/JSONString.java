c6b1445767500486df82abfc35c2a438aea9edf3
Riccardo Traverso <rtraverso86@gmail.com>
2014-08-01 09:54:44 +0200

    JSONString: better naming of methods, hashcode() and equals()

--- src/it/riccardotraverso/json/JSONString.java
+++ src/it/riccardotraverso/json/JSONString.java
@@ -2,24 +2,24 @@ package it.riccardotraverso.json;
 
 public class JSONString extends JSON {
 	
-	private String value;
+	private String text;
 
 	public JSONString(String value) {
-		this.value = value;
+		this.text = value;
 	}
 
 	/**
-	 * @return the value
+	 * @return the text
 	 */
-	public String getValue() {
-		return value;
+	public String getText() {
+		return text;
 	}
 
 	/**
-	 * @param value the value to set
+	 * @param text the text to set
 	 */
-	public void setValue(String value) {
-		this.value = value;
+	public void setText(String text) {
+		this.text = text;
 	}
 	
 	@Override
@@ -31,9 +31,42 @@ public class JSONString extends JSON {
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		builder.append('"');
-		builder.append(value);
+		builder.append(text);
 		builder.append('"');
 		return builder.toString();
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		JSONString other = (JSONString) obj;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		return true;
+	}
+
+	
+	
 }
