057f7c13797783ba94adcaf91bd3dabbac61735f
Riccardo Traverso <rtraverso86@gmail.com>
2014-08-11 15:50:16 +0200

    Removed unused main()

--- src/it/riccardotraverso/json/JSON.java
+++ src/it/riccardotraverso/json/JSON.java
@@ -10,22 +10,5 @@ public abstract class JSON {
 		this.accept(pp);
 		return pp.prettyPrint();
 	}
-	
-	public static void main(String[] args) {
-		JSONArray<JSON> js = new JSONArray<JSON>();
-		JSONObject obj = new JSONObject();
-		obj.setProperty("hello", new JSONString("world"));
-		obj.setProperty("name", new JSONBoolean(false));
-		js.addValue(obj);
-		js.addValue(new JSONInteger(2454));
-		JSONObject obj2 = new JSONObject();
-		obj2.setProperty("b", new JSONString("c"));
-		obj = new JSONObject();
-		obj.setProperty("1", js);
-		obj.setProperty("2", obj2);
-		obj.setProperty("3", new JSONObject());
-		obj.setProperty("4", new JSONArray<JSON>());
-		System.out.println(obj.toString());
-	}
 
 }
