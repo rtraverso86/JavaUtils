6a659e6d917a1f81bca0080ed7f98cc63a4cf6bd
Riccardo Traverso <rtraverso86@gmail.com>
2014-08-14 13:51:37 +0200

    New method: StringUtils.replicate(int, String)

--- src/it/riccardotraverso/java/lang/StringUtils.java
+++ src/it/riccardotraverso/java/lang/StringUtils.java
@@ -2,7 +2,6 @@ package it.riccardotraverso.java.lang;
 
 import it.riccardotraverso.java.util.ArraysUtils;
 
-import java.lang.String;
 import java.util.Iterator;
 
 public class StringUtils {
@@ -20,6 +19,36 @@ public class StringUtils {
 	 */
 	public static String replicate(int n, char c) {
 		return new String(ArraysUtils.replicate(n, c));
+	}
+
+	/**
+	 * Generates a string composed of n consecutive occurrences of the string s.
+	 * 
+	 * @param n
+	 * @param s
+	 * @return
+	 */
+	public static String replicate(int n, String s) {
+		java.lang.StringBuilder b = new java.lang.StringBuilder();
+		for (; n > 0; --n) {
+			b.append(s);
+		}
+		return b.toString();
+	}
+
+	/**
+	 * Generates a string composed of n consecutive occurrences of the string s.
+	 * 
+	 * @param n
+	 * @param s
+	 * @return
+	 */
+	public static String replicate(int n, String s) {
+		java.lang.StringBuilder b = new java.lang.StringBuilder();
+		for (; n > 0; --n) {
+			b.append(s);
+		}
+		return b.toString();
 	}
 
 	/**
@@ -41,7 +70,7 @@ public class StringUtils {
 	public static String nullAsEmpty(String s) {
 		return s == null ? "" : s;
 	}
-	
+
 	/**
 	 * Returns null when s is the empty string, or s as is otherwise.
 	 * 
@@ -49,9 +78,9 @@ public class StringUtils {
 	 * @return
 	 */
 	public static String emptyAsNull(String s) {
-		return isBlank(s) ? null : s; 
+		return isBlank(s) ? null : s;
 	}
-	
+
 	/**
 	 * Joins into a string an array of objects by using a separator.
 	 * 
@@ -60,7 +89,8 @@ public class StringUtils {
 	 * @return
 	 */
 	public static String join(Object[] arr, String separator) {
-		if (arr == null || arr.length < 1) return "";
+		if (arr == null || arr.length < 1)
+			return "";
 		StringBuilder sb = new StringBuilder(arr[0].toString());
 		for (int i = 1; i < arr.length; ++i) {
 			sb.append(separator);
@@ -68,18 +98,21 @@ public class StringUtils {
 		}
 		return sb.toString();
 	}
-	
+
 	/**
-	 * Joins into a string an iterable collection of objects by using a separator.
+	 * Joins into a string an iterable collection of objects by using a
+	 * separator.
 	 * 
 	 * @param coll
 	 * @param separator
 	 * @return
 	 */
 	public static <T> String join(Iterable<T> coll, String separator) {
-		if (coll == null) return "";
+		if (coll == null)
+			return "";
 		Iterator<T> it = coll.iterator();
-		if (!it.hasNext()) return "";
+		if (!it.hasNext())
+			return "";
 		StringBuilder sb = new StringBuilder(it.next().toString());
 		while (it.hasNext()) {
 			sb.append(separator);
@@ -88,9 +121,9 @@ public class StringUtils {
 		return sb.toString();
 	}
 
-	
 	/**
 	 * Capitalizes the very first character of a given string.
+	 * 
 	 * @param s
 	 * @return
 	 */
@@ -102,5 +135,5 @@ public class StringUtils {
 		builder.append(s.substring(1));
 		return builder.toString();
 	}
-	
+
 }
