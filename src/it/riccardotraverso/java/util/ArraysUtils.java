a5e796d0bd64dd047dddbc4fd1215d91f2c1165d
Riccardo Traverso <rtraverso86@gmail.com>
2014-08-14 13:57:14 +0200

    New overloaded methods: ArrayUtils.find() and ArrayUtils.contains()

--- src/it/riccardotraverso/java/util/ArraysUtils.java
+++ src/it/riccardotraverso/java/util/ArraysUtils.java
@@ -7,18 +7,215 @@ public class ArraysUtils {
 	private ArraysUtils() {
 		// Class not instantiable
 	}
-	
+
 	/**
 	 * Generates a string composed of just n consecutive occurrences of char c.
 	 * 
-	 * @param n 
-	 * @param c 
+	 * @param n
+	 * @param c
 	 * @return
 	 */
 	public static char[] replicate(int n, char c) {
 		char chars[] = new char[n];
 		Arrays.fill(chars, c);
 		return chars;
+	}
+
+	/**
+	 * Searches for an element in an array, returning its index if present. The
+	 * objects in the array are first compared by reference with the given
+	 * object, and then by means of the equals() method.
+	 * 
+	 * @param arr
+	 *            the array to search in
+	 * @param obj
+	 *            the object to search for in the given array
+	 * @return the index of the given object in the array if it is present, or
+	 *         -1 otherwise
+	 */
+	public static <T> int find(T[] arr, T obj) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			T t = arr[idx];
+			if (t == obj || t.equals(obj)) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(byte[] arr, byte b) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			byte t = arr[idx];
+			if (t == b) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(short[] arr, short s) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			short t = arr[idx];
+			if (t == s) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(boolean[] arr, boolean b) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			boolean t = arr[idx];
+			if (t == b) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(char[] arr, char c) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			char t = arr[idx];
+			if (t == c) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(int[] arr, int i) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			int t = arr[idx];
+			if (t == i) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(long[] arr, long l) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			long t = arr[idx];
+			if (t == l) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(float[] arr, float f) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			float t = arr[idx];
+			if (t == f) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+	
+	/**
+	 * @see #find(Object[], Object)
+	 */
+	public static int find(double[] arr, double d) {
+		for (int idx = 0; idx < arr.length; idx++) {
+			double t = arr[idx];
+			if (t == d) {
+				return idx;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * Tells whether an element is present in an array. The objects in the array
+	 * are first compared by reference with the given object, and then by means
+	 * of the equals() method.
+	 * 
+	 * @param arr
+	 *            the array to search in
+	 * @param obj
+	 *            the object to search for in the given array
+	 * @return true iff the object is present in the given array
+	 */
+	public static <T> boolean contains(T[] arr, T obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(byte[] arr, byte obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(short[] arr, short obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(boolean[] arr, boolean obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(char[] arr, char obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(int[] arr, int obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(long[] arr, long obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(float[] arr, float obj) {
+		return find(arr, obj) >= 0;
+	}
+	
+	/**
+	 *@see #contains(Object[], Object)
+	 */
+	public static boolean contains(double[] arr, double obj) {
+		return find(arr, obj) >= 0;
 	}
 
 }
