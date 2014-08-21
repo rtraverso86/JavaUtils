76480b439194ef5bbbfd82f99118cececa7e4f29
Riccardo Traverso <rtraverso86@gmail.com>
2014-08-21 17:24:32 +0200

    Added LGPL preamble to each file and to Readme

--- src/it/riccardotraverso/json/JSONBoolean.java
+++ src/it/riccardotraverso/json/JSONBoolean.java
@@ -1,3 +1,23 @@
+/*
+ * Copyright (C) 2014 Riccardo Traverso
+ * 
+ * This file is part of JavaUtils
+ * Website: https://github.com/rtraverso86/JavaUtils
+ * 
+ * JavaUtils is free software: you can redistribute it and/or modify 
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * JavaUtils is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package it.riccardotraverso.json;
 
 public class JSONBoolean extends JSON {
